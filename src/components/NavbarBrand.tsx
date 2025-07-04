ed91729f557e78a2fe1178c59128a2d4d6e8d434
nchicas224 <armando2095@hotmail.com>
2025-07-04 15:24:28 -0400

    Add: open LCF link in new tab, noopener noreferrer

--- src/components/NavbarBrand.tsx
+++ src/components/NavbarBrand.tsx
@@ -1,9 +1,13 @@
 import { Navbar } from "react-bootstrap";
 
-export default function NavbarLogo(){
-    return (
-        <Navbar.Brand href="https://office.com/search/?auth=2">
-            Living Classrooms Foundation
-        </Navbar.Brand>
-    )
+export default function NavbarLogo() {
+  return (
+    <Navbar.Brand
+      href="https://office.com/search/?auth=2"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Living Classrooms Foundation
+    </Navbar.Brand>
+  );
 }
