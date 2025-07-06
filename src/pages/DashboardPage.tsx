559134a10c8c73f50f29ea7e48f43610ab762612
nchicas224 <armando2095@hotmail.com>
2025-07-06 18:10:25 -0400

    Add: First name logic for greeting id in dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,11 +3,16 @@ import { useUser, getUserName } from "../hooks/UserContext";
 function DashboardPage() {
   const { user } = useUser();
   const name = getUserName(user);
+  const arr: string[] | undefined = name?.split(" ");
+  let firstName = "";
+  if (arr && arr.length){
+    firstName = arr[0];
+  } 
 
   return (
     <div>
       <h1 id="greeting">
-        Hello, { name ?? "Guest" }
+        Hello, { firstName ? firstName : "Guest" }
       </h1>
       <p>Welcome to your dashboard!</p>
     </div>
