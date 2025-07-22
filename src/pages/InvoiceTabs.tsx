c5e6fe1ac686397b1be3acce4247213fe52bc317
nchicas224 <armando2095@hotmail.com>
2025-07-22 19:07:53 -0400

    Change: Get invoice list JSON to JSX object shape

--- src/pages/InvoiceTabs.tsx
+++ src/pages/InvoiceTabs.tsx
@@ -2,26 +2,31 @@ import { useEffect, useState } from "react";
 import { getUser } from "../hooks/UserContext";
 import Table from "react-bootstrap/Table";
 
-interface InvoiceData {
-  id: string;
-  amount: string;
-  creation_time: string;
-  status: string;
-}
+// interface InvoiceData {
+//   id: string;
+//   amount: string;
+//   creation_time: string;
+//   status: string;
+// }
 
-interface InvoiceResp {
-  name: string;
-  values: InvoiceData;
-}
+// interface InvoiceResp {
+//   name: string;
+//   values: InvoiceData;
+// }
 
 interface Invoice {
-  name: string;
   id: string;
-  creation_time: string;
   status: string;
+  inv_name: string;
+  inv_num: string;
+  vendor: string;
+  amount: string;
+  inv_date: string;
+  due_date: string;
+  creation_date: string;
 }
 
-type InvoiceList = InvoiceResp[];
+//type InvoiceList = InvoiceResp[];
 
 export function InvoiceTab({ invTab }: { invTab: string }): React.ReactNode {
   const [data, setData] = useState<Invoice[] | null>(null);
@@ -30,16 +35,8 @@ export function InvoiceTab({ invTab }: { invTab: string }): React.ReactNode {
   useEffect(() => {
     if (!user) return;
     fetch(`/api/getInvoiceList?tab=${invTab}&currentUser=${user.userDetails}`)
-    .then(r => r.json() as Promise<InvoiceList>)
-    .then(data => {
-        const invoices: Invoice[] = data.map(
-          item => ({
-            name: item.name,
-            ...item.values,
-          })
-        );
-        setData(invoices);
-    })
+    .then(r => r.json() as Promise<Invoice[]>)
+    .then((data: Invoice[]) => setData(data))
     .catch(console.error)
   }, [invTab, user]);
 
@@ -52,7 +49,12 @@ export function InvoiceTab({ invTab }: { invTab: string }): React.ReactNode {
       <thead>
         <tr>
           <th>ID</th>
+          <th>Invoice #</th>
           <th>Invoice Name</th>
+          <th>Vendor</th>
+          <th>Amount</th>
+          <th>Invoice Date</th>
+          <th>Due Date</th>
           <th>Date Created</th>
           <th>Status</th>
         </tr>
@@ -61,8 +63,13 @@ export function InvoiceTab({ invTab }: { invTab: string }): React.ReactNode {
         {data.map(inv => (
           <tr>
             <td>{inv.id}</td>
-            <td>{inv.name}</td>
-            <td>{inv.creation_time}</td>
+            <td>{inv.inv_num}</td>
+            <td>{inv.inv_name}</td>
+            <td>{inv.vendor}</td>
+            <td>{inv.amount}</td>
+            <td>{inv.inv_date}</td>
+            <td>{inv.due_date}</td>
+            <td>{inv.creation_date}</td>
             <td>{inv.status}</td>
           </tr>
         ))}
