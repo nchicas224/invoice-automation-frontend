1dd1cc617d802276b7b7c9d41644883ce6765469
nchicas224 <armando2095@hotmail.com>
2025-09-30 15:31:30 -0400

    Try: change heirarchy of cassading sheets

--- src/pages/InvoicePage.tsx
+++ src/pages/InvoicePage.tsx
@@ -84,8 +84,8 @@ export function InvoicePage() {
   return (
     <Row className="g-3 w-100">
       <Col md={6} className="min-w-0">
-        <Tabs className="invoice-page-tabs-border">
-          <Tab eventKey="invoice" title="Invoice" className="bg-dark bg-gradient invoice-page-tabs">
+        <Tabs className="invoice-page-tabs-border invoice-page-tabs">
+          <Tab eventKey="invoice" title="Invoice">
             <Card className="h-100" style={{flexBasis: 0, minWidth: 0}}>
               <PdfViewer fileBytes={invoiceData} />
             </Card>
@@ -93,13 +93,13 @@ export function InvoicePage() {
         </Tabs>
       </Col>
       <Col md={6} className="min-w-0">
-        <Tabs activeKey={active} onSelect={(t) => setActive(t!)} className="invoice-page-tabs-border">
-          <Tab eventKey="checkRequest" title="Check Request" className="bg-dark bg-gradient invoice-page-tabs">
+        <Tabs activeKey={active} onSelect={(t) => setActive(t!)} className="invoice-page-tabs-border invoice-page-tabs">
+          <Tab eventKey="checkRequest" title="Check Request">
             <Card className="h-100" style={{flexBasis: 0, minWidth: 0}}>
               <PdfViewer fileBytes={checkData} />
             </Card>
           </Tab>
-          <Tab eventKey="editCheckRequest" title="Edit" className="bg-dark bg-gradient invoice-page-tabs">
+          <Tab eventKey="editCheckRequest" title="Edit">
             <Card className="h-100" style={{flexBasis: 0, minWidth: 0}}>
               <h1>EDIT FORM HERE</h1>
             </Card>
